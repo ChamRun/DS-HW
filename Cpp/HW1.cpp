a9b08764a51bf76019994f2fb678a9ab072ebb3c
chamRun <chrm@aut.ac.ir>
2021-10-16 14:59:55 +0330

    hw1 by cpp

---
+++ Cpp/HW1.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+ 
+using namespace std;
+ 
+
+int main()
+{
+
+    int n;
+    cin >> n;
+    
+    int first_digit;
+    cin >> first_digit;
+    int second_digit;
+    cin >> second_digit;
+
+    int numbs[n];
+
+    for (size_t i = 0; i < n; i++)
+    {
+        cin >> numbs[i];
+    }
+
+    int difference = counter(first_digit, 0, numbs);
+
+    cout<<"Hello World";
+     
+    return 0;
+}
+
+
+int counter(int first_digit, int difference, int numbs[]){
+
+    int n = 
+
+    switch (n)
+    {
+    case /* constant-expression */:
+        /* code */
+        break;
+    
+    default:
+        break;
+    }
+}
