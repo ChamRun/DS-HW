c76f870e2a6008a4049c5f93cf4accb2b47a7d47
chamRun <chrm@aut.ac.ir>
2021-11-07 13:36:41 +0330

    HW2

---
+++ Cpp/HW2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+
+    int t;
+    cin >> t;
+
+    for (size_t i = 0; i < t; i++)
+    {
+
+        int cou = 0;
+        int n;
+        cin >> n;
+        int arr[n];
+        for (size_t i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+
+        cout << cou;
+    }
+    
+}
