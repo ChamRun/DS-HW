1d5a6d26c2feb4e45d4ce720c6f5c9c21800ce3d
chamRun <chrm@aut.ac.ir>
2021-11-13 00:08:43 +0330

    maybe python: done...

--- Python/hw2v2.py
+++ Python/hw2v2.py
@@ -1,3 +1,5 @@
+import math
+
 if __name__ == '__main__':
     t = int(input())
 
@@ -7,15 +9,22 @@ if __name__ == '__main__':
         the_max = max(a)
 
         new_list = []
-        for j in range(the_max + 1):
+        for j in range(the_max):
             new_list.append(0)
 
-        print(a)
+        # print(a)
 
-        print("max: " + str(the_max))
+        # print("max: " + str(the_max))
 
         for number in a:
             # print("number == " + str(number) + ", new_list[number] == " + str(new_list[number]))
-            new_list[number] += 1
+            new_list[number - 1] += 1
 
-        print("new_list == " + new_list)
+        print("new_list == " + str(new_list))
+
+        counter = 0
+        for j in range(the_max):
+            counter += ((1 + new_list[j]) * new_list[j]) / 2
+
+        counter = math.trunc(counter)
+        print("ans: " + str(counter))
