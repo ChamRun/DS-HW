03786198dfb1651ec599afbb62ae5986495cb875
chamRun <chrm@aut.ac.ir>
2022-01-07 08:11:35 +0330

    DS-HW4: Completed

--- DS4/DS4Python/four.py
+++ DS4/DS4Python/four.py
@@ -1,2 +1,22 @@
+def find_longest_answer(array):
+    n = len(array)
+    dp = []
+
+    for i in range(len(array)):
+        maximum = 1
+
+        for j in range(i):
+            # print('checking ' + str(array[j]))
+            if array[j] < array[i] and maximum <= dp[j]:
+                # print(str(i) + ' comes after ' + str(j))
+                maximum = dp[j] + 1
+
+        dp.append(maximum)
+        print(str(array[i]) + ":" + str(dp[i]))
+
+    return max(dp)
+
+
 if __name__ == '__main__':
-    
+    answer = find_longest_answer([1, 2, 4, 3, 6, 7])
+    print(answer)
